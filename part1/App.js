88749c3d75639a4a5549bcef5f317e14ab257c89
thanatly <82842370+thanatly@users.noreply.github.com>
2021-05-08 00:07:49 +0300

    Add Ex 1.4
    Refactor Objects as an Array

--- part1/App.js
+++ part1/App.js
@@ -3,18 +3,20 @@ import React from 'react'
 // const-definitions
 const course = 'Half Stack application development'
   // define as Object
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
 const Header = (props) => {
   return (
@@ -39,9 +41,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={part1.name} exercise={part1.exercises}/>
-      <Part part={part2.name} exercise={part2.exercises}/>
-      <Part part={part3.name} exercise={part3.exercises}/>
+      <Part part={parts[0].name} exercise={parts[0].exercises}/>
+      <Part part={parts[1].name} exercise={parts[1].exercises}/>
+      <Part part={parts[2].name} exercise={parts[2].exercises}/>
     </div>
   )
 }
@@ -61,7 +63,7 @@ const App = () => {
     <div>
       <Header course={course}/>
       <Content />
-      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   )
 }
