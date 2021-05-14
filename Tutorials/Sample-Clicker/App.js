df7b23842cc600c615dbaa6f0c2aa3816dc3e6b7
thanatly <82842370+thanatly@users.noreply.github.com>
2021-05-14 20:14:56 +0300

    Add Left and Right clicker (complex state)

--- Tutorials/Sample-Clicker/App.js
+++ Tutorials/Sample-Clicker/App.js
@@ -1,40 +1,35 @@
 // Clicker app - follow a tutorial
-// Concept: Event , Destructuring
+// Concept: Complex state
 
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  const increaseByOne = () => setCounter(counter + 1)
-  
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = () => {
+  const newClicks = { 
+    ...clicks, //Spread syntax
+    left: clicks.left + 1 
+  }
+  setClicks(newClicks)
+}
 
-  const decreaseByOne = () => setCounter(counter - 1)
-
-  const Display = ({ counter }) => <div>{counter}</div>
-
-  const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+const handleRightClick = () => {
+  const newClicks = { 
+    ...clicks, 
+    right: clicks.right + 1 
+  }
+  setClicks(newClicks)
+}
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        handleClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        handleClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        handleClick={decreaseByOne}
-        text='minus'
-      />   
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 }
