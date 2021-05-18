d4a250b12147d2f1515cc15413d98f8e06f25736
thanatly <82842370+thanatly@users.noreply.github.com>
2021-05-18 13:13:31 +0300

    Add ex 1.9 unicafe

--- part1/unicafe/App.js
+++ part1/unicafe/App.js
@@ -7,32 +7,50 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Statistics = (props) => {
+  //Helper component
+  const all = () => props.good + props.neutral + props.bad
+
+  if (all() === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+    <ul>
+      <li> Good: {props.good} </li>
+      <li> Neutral: {props.neutral} </li>
+      <li> Bad: {props.bad} </li>
+      <li> All: {all()} </li>
+      <li> Average: {(props.good - props.bad)/all()} </li>
+      <li> Positive: {100*props.good/all()} % </li>    
+    </ul>  
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   // I feel like there is a better solution than setting new states :/
-  const [score, setScore] = useState(0) 
-  const [click, setClick] = useState(0)
-
+  const [allClicks, setAll] = useState([])
 
   const handleGoodClick = () => {
     setGood(good + 1)
-    setScore(score + 1)
-    setClick(click + 1)
-
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
-    setClick(click + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
-    setScore(score - 1)
-    setClick(click + 1)
   }
 
  
@@ -43,14 +61,7 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text='Neutral' />
       <Button handleClick={handleBadClick} text='Bad' />
     <h2>Statistics</h2>
-    <ul>
-      <li> Good: {good} </li>
-      <li> Neutral: {neutral} </li>
-      <li> Bad: {bad} </li>
-      <li> All: {click} </li>
-      <li> Average: {score/click} </li>
-      <li> Positive: {100*good/click} % </li>
-    </ul>
+      <Statistics good={good} neutral={neutral} bad={bad} allClicks={allClicks} />
     </div>
   )
 }
