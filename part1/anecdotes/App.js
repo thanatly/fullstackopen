44c9bd46531f41d58d28ac35f6feb8b789ca9ee6
thanatly <82842370+thanatly@users.noreply.github.com>
2021-05-19 15:52:04 +0300

    Add ex 1.3 anecdotes
    Store voting score of the quote in an array

--- part1/anecdotes/App.js
+++ part1/anecdotes/App.js
@@ -1,4 +1,3 @@
-// Anecdotes
 import React, { useState } from 'react'
 
 const Button = (props) => (
@@ -14,27 +13,39 @@ const App = () => {
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
-   
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'   
+  ]  
+  const initialVote = new Array(anecdotes.length).fill(0)
+
+  const [vote, setVote] = React.useState(initialVote);
+
   const [selected, setSelected] = useState(0)
 
   const randomText = () =>{ 
-    const randomNumber = Math.floor(Math.random() * 6);
+    const randomNumber = Math.floor(Math.random() * anecdotes.length);
 
     setSelected(randomNumber)
-    console.log(randomNumber)
+  }
+
+  const clickVote = () => { 
+    const updatedVote = [...vote]
+    updatedVote[selected]+= 1 
+    setVote(updatedVote)
   }
 
   return (
     <div>
+    <p>
       {anecdotes[selected]}
-      <Button handleClick={randomText} text= "Next" />
+    </p>
+    <p>
+      has {vote[selected]} votes
+    </p> 
+    <Button handleClick={clickVote} text= "Vote" />
+    <Button handleClick={randomText} text= "Next" />
     </div>
 
   )
 }
 
-
-
-export default App
+export default App 
