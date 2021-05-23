83898d6815459f19b6d44ec71276231d60104262
thanatly <82842370+thanatly@users.noreply.github.com>
2021-05-23 14:01:14 +0300

    Refactor a component via destructuring

---
+++ App.js
@@ -0,0 +1,22 @@
+// Concept: functional programing- map
+import React from 'react'
+
+//Destructuring
+const App = ({ notes }) => {
+  return (
+    // The value of the variable must be rendered inside of curly braces.
+    // If not, the parameter becomes a plain text
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id}>
+            {note.content} 
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
