6185241fdb1c92947d4bbc1f3df2c0dc28296914
thanatly <82842370+thanatly@users.noreply.github.com>
2021-05-24 19:26:42 +0300

    Add ex 2.7 to support any numbers of courses

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
 
 const Content = ({ course }) => {
+  const headers = course.name
   const parts = course.parts
-
-  // Calculate total exercises
-  // Need to specify InitialValue as 0 in reducer 
   const exTotal = parts.reduce((s, p) => s + p.exercises,0);
-
-  // Print out Total in the console
-  console.log({exTotal})
 
   return (
     <div>
+      <h1>{headers}</h1>
       {parts.map(part =>
         <p key={part.id}>
         {part.name} {part.exercises}
@@ -29,36 +20,66 @@ const Content = ({ course }) => {
   )
 }
 
+// Iterate over an array of courses
+const Display = ({ courses }) => {
+    return(
+      courses.map((course,index) => <Content course={course} 
+        key={course.id}/>)
+      )
+}
+
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   //Check if the app is working
   console.log('App works...')
 
   return (
   <div>
-  <Header course={course} />
-  <Content course={course} />
+  <Display courses={courses}/>
   </div>
   )
 }
