e68d42da13dc909be7e35e0d67d3d45e7d2d31be
thanatly <82842370+thanatly@users.noreply.github.com>
2021-05-25 17:40:28 +0300

    Add showAll filter

--- Tutorials/Sample-Map/App.js
+++ Tutorials/Sample-Map/App.js
@@ -1,13 +1,12 @@
-// Concept: Controlled components
+// Concept: Filter
 import React, { useState } from 'react'
 import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
 
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
+  const [newNote, setNewNote] = useState('') 
+  const [showAll, setShowAll] = useState(true)
 
 const addNote = (event) => {
   event.preventDefault()
@@ -29,11 +28,23 @@ const addNote = (event) => {
     setNewNote(event.target.value)
   }
 
+  //const result = condition 
+  //? val1 
+  //: val2
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
